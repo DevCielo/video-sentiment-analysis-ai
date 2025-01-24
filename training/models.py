5fcbce4de25e867d7762a3aaa9858ee972efe2c0
DevCielo <136787904+DevCielo@users.noreply.github.com>
2025-01-24 17:19:36 +1000

    created the video encoder

--- training/models.py
+++ training/models.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
 from transformers import BertModel
+from torchvision import models as vision_models
 
 class TextEncoder(nn.Module):
     def __init__(self):
@@ -10,7 +11,7 @@ class TextEncoder(nn.Module):
         for param in self.bert.parameters():
             param.requires_grad = False
 
-        # Outputs 768 which is then converted to 128 to make summary smaller
+        # BERT Outputs 768 which is then converted to 128 to make summary smaller
         self.projection = nn.Linear(768, 128)
 
     def forward(self, input_ids, attention_mask):
@@ -22,4 +23,27 @@ class TextEncoder(nn.Module):
 
         return self.projection(pooler_output)
 
+class VideoEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        # Get the ResNet 3D 18 layers pretrained model
+        self.backbone = vision_models.video.r3d_18(pretrained=True)
 
+        # Sets all layers to not be training
+        for param in self.backbone.parameters():
+            param.requires_grad = False
+
+        num_fts = self.backbone.fc.in_features
+
+        # Defines the fc (classification head) so only the linear layer is trainable
+        self.backbone.fc = nn.Sequential(
+            nn.Linear(num_fts, 128),
+            nn.ReLU(),
+            nn.Dropout(0.2)
+        )
+    
+    def forward(self, x):
+        # x represents the entire tensor that contains all the video frames
+        # [batch_size, frames, channels, height, width] -> [batch_size, channels, frames, height, width]
+        x = x.transpose(1, 2)
+        return self.backbone(x)
