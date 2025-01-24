9e9c1a10da400fece0e9950d0e7265393be13efd
DevCielo <136787904+DevCielo@users.noreply.github.com>
2025-01-24 18:51:27 +1000

    finished the audio encoder

--- training/models.py
+++ training/models.py
@@ -1,6 +1,7 @@
 import torch.nn as nn
 from transformers import BertModel
 from torchvision import models as vision_models
+import torch
 
 class TextEncoder(nn.Module):
     def __init__(self):
@@ -47,3 +48,39 @@ class VideoEncoder(nn.Module):
         # [batch_size, frames, channels, height, width] -> [batch_size, channels, frames, height, width]
         x = x.transpose(1, 2)
         return self.backbone(x)
+
+class AudioEncoder(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv_layers = nn.Sequential(
+            # Lower level features
+            nn.Conv1d(64, 64, kernel_size=3),
+            nn.BatchNorm1d(64),
+            nn.ReLU(),
+            nn.MaxPool1d(2),
+            # Higher level features
+            nn.Conv1d(64, 128, kernel_size=3),
+            nn.BatchNorm1d(128),
+            nn.ReLU(),
+            nn.AdaptiveAvgPool1d(1),
+        )
+
+        for param in self.conv_layers.parameters():
+            param.requires_grad = False
+
+        self.projection = nn.Sequential(
+            nn.Linear(128, 128),
+            nn.ReLU(),
+            nn.Dropout(0.2)
+        )
+
+    def forward(self, x):
+        # [batch_size, 1, 64 (frequencies (y-axis)), 300 (time (x-axis))] -> [batch_size, 64, 300]
+        x = x.squeeze(1)
+
+        # Run through conv layers
+        features = self.conv_layers(x)
+
+        # Features output: [batch_size, 128, 1]
+
+        return self.projection(features.squeeze(-1))
